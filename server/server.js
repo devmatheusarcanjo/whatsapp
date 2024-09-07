12dd5efa8bb8877df0755675708b82086f266d68
Matheus Souza <matheussouzaarcanjo444@gmail.com>
2024-09-07 14:24:15 -0300

    Alteração em server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { grupoConnection } = require('./grupoConnection.js');
 
 const app = express();
 const server = http.createServer(app);
